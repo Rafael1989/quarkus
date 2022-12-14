b456f94b4b931a5c72e82ebf55d8b2bf8f00af34
Rafael Bertoni <rrbertoni21071989@gmail.com>
2022-12-15 00:06:30 +0100

    First commit

---
+++ src/test/java/org/agoncal/quarkus/starting/BookResourceTest.java
@@ -0,0 +1,20 @@
+package org.agoncal.quarkus.starting;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+public class BookResourceTest {
+
+    @Test
+    public void testHelloEndpoint(){
+        given().
+        when()
+            .get("/api/books").
+        then()
+            .statusCode(200)
+            .body(is("Hello RestEasy"));
+
+    }
+}
